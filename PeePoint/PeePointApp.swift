3301a5d5015b931aed2fcc8cf15512af1e14e276
REOysd <202yyrr@gmail.com>
2024-08-24 23:42:46 +0900

    コンフリクト解消

--- PeePoint/PeePointApp.swift
+++ PeePoint/PeePointApp.swift
@@ -11,13 +11,8 @@ import SwiftUI
 struct PeePointApp: App {
     var body: some Scene {
         WindowGroup {
-<<<<<<< HEAD
-            defaultSheetView()
-=======
             MainMapView()
->>>>>>> 6869a9af046e0b36d3d649bf91ba5ac1d2264c0d
                 .environmentObject(PublicToiletManager())
-                                   
         }
     }
 }
