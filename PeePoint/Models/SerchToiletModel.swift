e3b4b724222d9d9e3dd47377434a111dd60875fc
REOysd <202yyrr@gmail.com>
2024-08-24 20:21:34 +0900

    SerchToiletModel作成

---
+++ PeePoint/Models/SerchToiletModel.swift
@@ -0,0 +1,27 @@
+//
+//  SerchToiletModel.swift
+//  PeePoint
+//
+//  Created by 吉田怜央 on 2024/08/24.
+//
+
+import SwiftUI
+import CoreLocation
+
+class ToieltSerchByNameModel : NSObject,ObservableObject,CLLocationManagerDelegate {
+    func toiletSerch(publicToilet:PublicToilet,boundary: CGRect)-> [PublicToilet] {
+        let quadtree = QuadtreeNode(boundary: boundary, capacity: 50)
+        quadtree.insert(toilet: publicToilet)
+
+    }
+
+    private func requestLocation()-> CLLocationManager{
+        let locationManager = CLLocationManager()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestLocation()
+        }
+        return locationManager
+    }
+}
