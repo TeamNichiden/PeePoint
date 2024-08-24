ac2d4be8b2a661330fc88b66fa268e6735867f1b
yukiimai <dlxsmd@icloud.com>
2024-08-24 11:04:49 +0900

    Initial Commit

---
+++ PeePoint/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  PeePoint
+//
+//  Created by Yuki Imai on 2024/08/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Map()
+       .ignoresSafeArea(edges: .top)
+        .frame(height: 300)
+      VStack(alignment: .leading) {
+
+        
+         Text("Turtle Rock")
+          .font(.title)
+         HStack {
+          Text("Joshua Tree National Park")
+            .font(.subheadline)
+          Spacer()
+          Text("California")
+            .font(.subheadline)
+        }
+        .font(.subheadline)
+        .foregroundColor(.secondary)
+        Divider()
+        Text("About Turtle Rock")
+          .font(.title2)
+        Text("Descriptive text goes here.")
+      }
+      .padding()
+      Spacer()
+    }
+  }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeePoint/PeePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeePointApp.swift
+//  PeePoint
+//
+//  Created by Yuki Imai on 2024/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PeePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
