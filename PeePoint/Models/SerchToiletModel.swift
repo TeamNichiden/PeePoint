fb6da4d5f785852787b5443cdb19fc8ac0d04712
REOysd <202yyrr@gmail.com>
2024-08-25 02:56:38 +0900

    save

--- PeePoint/Models/SerchToiletModel.swift
+++ PeePoint/Models/SerchToiletModel.swift
@@ -8,30 +8,32 @@
 import SwiftUI
 import CoreLocation
 
-//class ToieltSerchByNameModel : NSObject,ObservableObject,CLLocationManagerDelegate {
-//    private var currentLocation = CLLocation(latitude: 139.6917, longitude: 35.6895)// 東京駅付近
-//    private var quadtree = PublicToiletManager()
-//    override init() {
-//        super .init()
-//        requestLocation()
-//    }
-//
-//    func toiletSerch() {
-//        quadtree.findNearestToilets(currentLocation: currentLocation, maxResults: 20)
-//    }
-//
-//    private func requestLocation(){
-//        let locationManager = CLLocationManager()
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.requestWhenInUseAuthorization()
-//
-//        if CLLocationManager.locationServicesEnabled() {
-//            locationManager.requestWhenInUseAuthorization()
-//            locationManager.requestLocation()
-//        }
-//        guard let currentLocation = locationManager.location else {
-//            return
-//        }
-//    }
-//}
+class SerchToiletModel : NSObject,ObservableObject,CLLocationManagerDelegate {
+    @Published var currentLocation = CLLocation(latitude: 139.6917, longitude: 35.6895)// 東京駅付近
+    private var quadtree = PublicToiletManager()
+
+    override init() {
+        super .init()
+        requestLocation()
+    }
+
+    func toiletSerch() {
+        quadtree.findNearestToilets(currentLocation: currentLocation, maxResults: 20)
+    }
+
+    private func requestLocation(){
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestLocation()
+        }
+        guard let _currentLocation = locationManager.location else {
+            return
+        }
+        self.currentLocation = _currentLocation
+    }
+}
 
