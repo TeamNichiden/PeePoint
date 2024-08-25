7afb3e19eeaa68fddaf5032e312a8cc633775e15
アウン <hlumaungphyo@gmail.com>
2024-08-25 10:55:59 +0900

    defaultSheetViewのバグ修正

--- PeePoint/UI/SheetViews/defaultSheetView.swift
+++ PeePoint/UI/SheetViews/defaultSheetView.swift
@@ -8,54 +8,70 @@
 import SwiftUI
 
 struct defaultSheetView: View {
+    @Binding var showNearbyToiletSheet : Bool
+    @Binding var selectedToilet: PublicToilet?
     @EnvironmentObject var toiletData:PublicToiletManager
-    
+    @Binding var showDetailView:Bool
     var body: some View {
         VStack(alignment: .leading) {
             Text("お近くのトイレ")
                 .font(.title)
-                .padding(.bottom)
+                .padding(.vertical)
             
-            ScrollView{
-                ForEach(toiletData.nearestToilets, id: \.self) { toilet in
-                    HStack {
+            
+            ForEach(toiletData.nearestToilets, id: \.self) { toilet in
+                HStack {
+                    Group{
                         Image("toilet-thumbnil")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width:50)
                             .cornerRadius(5)
-
+                        
                         VStack(alignment: .leading) {
                             Text(toilet.address ?? "名前なし")
                                 .font(.headline)
+                                .frame(width: 150,alignment: .leading)
                             Text("徒歩 : 15分")
                                 .font(.system(size:13))
                         }
-                        Spacer()
-                        //案内スタート
-                        Button(action: {
-                            //
-                        }) {
-                            Text("Direction")
-                                .foregroundColor(.white)
-                                .font(.headline)
-                                .frame(maxWidth: .infinity)
-                                .frame(height:48)
-                                .background(Color.customColor)
-                                .cornerRadius(10)
-                                .padding(.leading)
-                        }
                     }
-                    .padding()
-                    .background(Color(hue: 1.0, saturation: 0.002, brightness: 0.863))
-                    .cornerRadius(15)
+                    .onTapGesture {
+                        selectedToilet = toilet
+                        showNearbyToiletSheet = false
+                        showDetailView = true
+                    }
+                    
+                    Spacer()
+                    
+                    //案内スタート
+                    Button(action: {
+                        //
+                    }) {
+                        Text("Direction")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .frame(height:48)
+                            .background(Color.customColor)
+                            .cornerRadius(10)
+                            .padding(.leading)
+                            .shadow(radius:10, y:5)
+                    }
                 }
+                .padding()
+                .background(Color(hue: 1.0, saturation: 0.002, brightness: 0.863))
+                .cornerRadius(15)
+                
             }
         }
         .padding()
-
+        
     }
+    
+    
 }
+
 
 extension Color {
     static var customColor:Color {
@@ -63,7 +79,10 @@ extension Color {
     }
 }
 
-#Preview{
-    defaultSheetView()
-        .environmentObject(PublicToiletManager())
-}
+//#Preview{
+//    @Previewable @State var sheetIndex: Int = 0
+//    NavigationStack{
+//        defaultSheetView(showNearbyToiletSheet: .constant(true), selectedToilet: , showDetailView: .constant(false))
+//            .environmentObject(PublicToiletManager())
+//    }
+//}
